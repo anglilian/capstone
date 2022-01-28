94540fd5148e4160019681c45109cf511265f3a6
Ang Li-Lian <43257301+anglilian@users.noreply.github.com>
2022-01-29 05:34:55 +0800

    Fix footnotes

--- website/assets/js/footnote.js
+++ website/assets/js/footnote.js
@@ -8,25 +8,27 @@ var ID;
 var check_pos = function() {
     var middle_of_screen = $(window).height()/2 + $(window).scrollTop();
     var button = $("[data-footnote-identifier="+ID+"]");  
+    var buttonDim = button[0].getBoundingClientRect();
+    $(".footnote-tip").css("left", $("aside").width()/2);
     if (button.offset().top > middle_of_screen){
-      $("aside").css("bottom", $(window).height()-button[0].getBoundingClientRect().bottom+10);
+      $("aside").css("bottom", $(window).height()-buttonDim.bottom+10);
       $("aside").css("top", "auto");
-                $(".footnote-tip").css("top", $("aside").height()-10);
+        $(".footnote-tip").css("top", $("#footnote-div").height()+20);
         $(".footnote-tip").css("box-shadow", "4px 2px 2px 0px rgba(0,0,0,0.2)");
     } else {
-      $("aside").css("top", button[0].getBoundingClientRect().bottom+10); 
+      $("aside").css("top", buttonDim.bottom+10); 
       $("aside").css("bottom", "auto");
-                $(".footnote-tip").css("top", -10);
+        $(".footnote-tip").css("top", -10);
         $(".footnote-tip").css("box-shadow", "-4px -2px 2px 0px rgba(0,0,0,0.2)");
     }
-    var left_of_footnote = button[0].getBoundingClientRect().left - $("aside").width()/2;
-    var right_of_footnote = button[0].getBoundingClientRect().right + $("aside").width()/2;  
+    var left_of_footnote = buttonDim.left - $("aside").width()/2;
+    var right_of_footnote = buttonDim.right + $("aside").width()/2;  
     if (left_of_footnote < 0){
         $("aside").css("left", 10);
     } else if (right_of_footnote > $(window).width()){
         $("aside").css("left", $(window).width() - $("aside").width()-10);
     } else{
-        $("aside").css("left", button[0].getBoundingClientRect().left - $("aside").width()/2);
+        $("aside").css("left", buttonDim.left - $("aside").width()/2);
     }
 };
 
